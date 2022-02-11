62da3cfb4ccc82e81284e39d40d92123d54bf148
ThiagoSS1 <thiago_seta@hotmail.com>
2022-02-11 10:12:58 -0300

    atualizando frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,54 +1,53 @@
 const api = axios.create({
   baseURL: 'https://backend-api-banco.herokuapp.com/'
+  //baseURLDev: 'http://localhost:8080'
 })
+
+class User {
+  constructor(name, senha, repeatPass) {
+    this.name = name;
+    this.password = senha;
+    this.repeatPass = repeatPass;
+  }
+}
 
 
 preencherLinha();
 
 async function criarUsuario () {
   const form = document.getElementById("form1");
-  const nome = form.elements["user"].value;
+  const name = form.elements["user"].value;
   const senha = form.elements["password"].value;
   const repeatPass = form.elements["reapeat-pass"].value
+  const user = new User(name, senha, repeatPass)
 
-  if (nome && senha) {
-    const lsUsers = await api.get(`/users`).then((response) => {
-      return response.data
-    })
-    if (!lsUsers.find((user) => user.nome === nome)) {
-      const user = {
-        name: nome,
-        password: senha,
-        repeatPass
-      };
-      await api.post(`/users`, user).then((response) => {
-        alert("usuario criado com exito");
-        window.location.replace("index.html");
-      }).catch((error) => {
-        console.error(error);
-      })
-    } else {
-      alert("Usuário já existente.");
-    }
-  }
+  await api.post(`/users`, user).then((response) => {
+
+    alert("usuario criado com exito");
+
+    window.location.replace("index.html");
+
+
+  }).catch((error) => {
+    console.error(error.response.data);
+  })
+
 }
 
 // botao de entrar
 async function login () {
-  
+
   const form = document.getElementById("formlogin");
   const nome = form.elements["user"].value;
   const senha = form.elements["password"].value;
 
-  const lsUsers = await api.get(`/users`).then((response) => {
-    return response.data
-  })
-
   // o sinal de interrogação verifica se o objeto pai é diferente de null, false ou undefined
   if (!!nome && !!senha) {
-    await api.post(`/login`, { user: nome, password: senha }).then((response) => {
+    await api.post(`/login`, { name: nome, password: senha }).then((response) => {
       alert("Você está logado!");
-      localStorage.setItem("userId", JSON.stringify(response.data.id));
+
+      localStorage.setItem("userId", response.data.uid);
+
       window.location.replace("index3.html");
     }).catch((error) => {
       console.error(error);
@@ -58,20 +57,8 @@ async function login () {
   }
 }
 
-function montarRecados () {
-  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  if (loggedUser) {
-    const users = JSON.parse(localStorage.getItem("users"));
-    const userObj = users.find((user) => user.nome === loggedUser.nome);
-    if (!!userObj.descricoes && !!userObj.detalhamentos) {
-      userObj.descricoes.forEach((descricao, index) =>
-        preencherLinha(index, descricao, userObj.detalhamentos[index])
-      );
-    }
-  }
-}
+async function adicionarRecado () {
 
-function adicionarRecado ( description, details) {
   const descriptionInput = document.getElementById("description").value;
   const detailsInput = document.getElementById("details").value;
   const id = localStorage.getItem("userId");
@@ -79,77 +66,91 @@ function adicionarRecado ( description, details) {
   if (!!!descriptionInput && !!!detailsInput) {
     return alert("Preencha os campos");
   } else {
-    await api.post(`/messages/${JSON.parse(id)}`).then((response) => {
-      return response.data.data
-    }) 
+    await api.post(`/messages/${id}`, { title: descriptionInput, description: detailsInput }).then((response) => {
+      alert("mesagem criada com exito");
+      console.log(response)
+    }).catch((error) => {
+      console.error(error);
+    })
   }
-
-
 
   return preencherLinha();
 }
 
-async function preencherLinha () {
-  const tbody = table.querySelector("tbody");
-  const id = localStorage.getItem("userId");
-  // Criar recados
-  const recados = await api.get(`/messages/${JSON.parse(id)}`).then((response) => {
-    return response.data.data
+async function preencherLinha (pos) {
+  const uid = pos
+  const ids = localStorage.getItem("userId")
+
+  //Criar recados
+  await api.get(`/messages/${ids}`).then((response) => {
+
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+
+    for (let i = 0; i < response.data.length; i++) {
+
+      const uid = response.data[i].uid
+      console.log(uid)
+      const row = tbody.insertRow();
+      const ids = row.insertCell(0);
+      const titles = row.insertCell(1);
+      const descriptions = row.insertCell(2);
+      const actions = row.insertCell(3);
+
+      ids.innerHTML = response.data.indexOf(response.data[i]) + 1;
+      titles.innerHTML = response.data[i].title;
+      descriptions.innerHTML = response.data[i].description;
+      actions.innerHTML = `<button onclick="editar('${uid}')" class="btn btn-primary mx-2">Editar</button><button onclick="apagarMensagem('${uid}')" class="btn btn-danger mx-2">Excluir</button>`;
+
+    }
+
   })
-
-  tbody.innerHTML = "";
-  
-  recados?.forEach((recado, index) => {
-    const row = tbody.insertRow();
-    const id = row.insertCell(0);
-    const description = row.insertCell(1);
-    const details = row.insertCell(2);
-    const actions = row.insertCell(3);
-
-    id.innerHTML = index + 1;
-    description.innerHTML = recado.description;
-    details.innerHTML = recado.details;
-    actions.innerHTML = `<button onclick="editar(${index})" class="btn btn-primary mx-2">Editar</button><button class="btn btn-danger mx-2">Excluir</button>`;
-  });
-
 }
 
-function editar (indice) {
+
+async function editar (uid_message) {
   $("#exampleModalCenter").modal("show");
 
-  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  const users = JSON.parse(localStorage.getItem("users"));
-  const userIndex = users.findIndex((user) => user?.nome === loggedUser?.nome);
+  const ids = localStorage.getItem("userId")
 
   const descriptionEdit = document.getElementById("descriptionEdit");
   const detailsEdit = document.getElementById("detailsEdit");
 
-  const buttonSaveEdit = document.getElementById("buttonSaveEdit");
+  let buttonSaveEdit = document.getElementById("buttonSaveEdit");
 
-  const recadoParaEdicao = users[userIndex].recados[indice];
+  buttonSaveEdit.addEventListener("click", async () => {
+    let descriptionEdit = document.getElementById("descriptionEdit").value;
+    let detailsEdit = document.getElementById("detailsEdit").value;
 
-  descriptionEdit.value = recadoParaEdicao.description;
-  detailsEdit.value = recadoParaEdicao.details;
-
-  buttonSaveEdit.onclick = () => {
     if (!!!descriptionEdit && !!!detailsEdit) {
       return alert("Preencha os campos");
+    } else {
+      await api.put(`messages/${ids}/${uid_message}`, { title: descriptionEdit, description: detailsEdit })
+        .then((response) => {
+
+          alert("mesagem editada com exito");
+
+        })
     }
 
-    let recado = {
-      description: descriptionEdit.value,
-      details: detailsEdit.value,
-    };
+    // editar recado
 
-    console.log(recado);
-
-    users[userIndex].recados[indice] = recado;
-
-    //editar recado
-    
     $("#exampleModalCenter").modal("hide");
     preencherLinha();
-  };
+  })
+}
+
+async function apagarMensagem (uid) {
+  let id = uid
+  const ids = localStorage.getItem("userId")
+
+  await api.delete(`/messages/${ids}/${id}`).then((response) => {
+    alert("Mensagem apagada com exito");
+    console.log(response.data)
+  }).catch((error) => {
+    console.error(error);
+  })
+  preencherLinha();
 }
 
 function deslogar () {
@@ -161,12 +162,7 @@ async function verificaUsuarioLogado () {
   const id = localStorage.getItem("userId");
 
   if (id != null) {
-    return await api.get(`/users/${JSON.parse(id)}`).then((response) => {
-    }).catch((error) => {
-      console.error(error);
-      alert("Você precisa estar logado para acessar essa página");
-      window.location.replace("index.html");
-    })
+    return
 
   }
   alert("Você precisa estar logado para acessar essa página");
@@ -176,6 +172,5 @@ async function verificaUsuarioLogado () {
 if (window.location.pathname.includes('index3')) {
   verificaUsuarioLogado();
 }
-
 
 
